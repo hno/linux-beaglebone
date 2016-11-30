99a878b8ae62cf70a08da8430cd1dec4a3183cd4
Henrik Nordström <henrik.nordstrom@borderlight.net>
2016-11-30 18:29:52 +0100

    pruss rproc: Allow specifying firmware name in devicetree tw,pruss-firmware

--- drivers/remoteproc/pru_rproc.c
+++ drivers/remoteproc/pru_rproc.c
@@ -721,6 +721,7 @@ static int pru_rproc_probe(struct platform_device *pdev)
 	const char *mem_names[PRU_MEM_MAX] = { "iram", "control", "debug" };
 	bool use_eth = false;
 	u32 mux_sel;
+	const char *fw_name;
 
 	if (!np) {
 		dev_err(dev, "Non-DT platform device not supported\n");
@@ -746,8 +747,18 @@ static int pru_rproc_probe(struct platform_device *pdev)
 			use_eth = true;
 	}
 
-	rproc = rproc_alloc(dev, pdev->name, &pru_rproc_ops,
-			    (use_eth ? pdata->eth_fw_name : pdata->fw_name),
+	if (!use_eth)
+		ret = of_property_read_string(dev->of_node, "ti,pruss-firmware",
+					      &fw_name);
+	else
+		ret = of_property_read_string(dev->of_node, "ti,eth-firmware",
+					      &fw_name);
+	if (ret) {
+		dev_info(dev, "No firmware filename given in dt\n");
+		fw_name = use_eth ? pdata->eth_fw_name : pdata->fw_name;
+	}
+
+	rproc = rproc_alloc(dev, pdev->name, &pru_rproc_ops, fw_name,
 			    sizeof(*pru));
 	if (!rproc) {
 		dev_err(dev, "rproc_alloc failed\n");
@@ -760,7 +771,7 @@ static int pru_rproc_probe(struct platform_device *pdev)
 	pru->id = pdata->id;
 	pru->pruss = platform_get_drvdata(ppdev);
 	pru->rproc = rproc;
-	pru->fw_name = use_eth ? pdata->eth_fw_name : pdata->fw_name;
+	pru->fw_name = fw_name;
 	pru->use_eth = use_eth;
 	spin_lock_init(&pru->rmw_lock);
 
